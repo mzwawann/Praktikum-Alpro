9ca2103fb8db413063f112e2b026fdb0e959ee99
kurniawannn <kurniawanfirdaus2806@gmail.com>
2026-03-16 08:35:56 +0700

    71251186

--- Pertemuan 06 - STRUKTUR KONTROL PERULANGAN/Latihan_6.1.py
+++ Pertemuan 06 - STRUKTUR KONTROL PERULANGAN/Latihan_6.1.py
@@ -1,12 +1,18 @@
 def perkalian(a, b):
     hasil = 0
+    
+    print(a, "x", b, "=", end=" ")
+    
     for i in range(a):
         hasil = hasil + b
-    return hasil
+        print(b, end="")
+        
+        if i < a-1:
+            print(" + ", end="")
+    
+    print(" =", hasil)
 
-angka1 = int(input("Masukkan angka pertama: "))
-angka2 = int(input("Masukkan angka kedua: "))
+a = int(input("Masukkan angka pertama: "))
+b = int(input("Masukkan angka kedua: "))
 
-hasil = perkalian(angka1, angka2)
-print(angka1, "x", angka2, "=", hasil)
-
+perkalian(a, b)
