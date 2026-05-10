50d739b862e86de9b66aeb31f8f920a7ceec0d46
kurniawannn <kurniawanfirdaus2806@gmail.com>
2026-05-10 16:38:39 +0700

    71251186

--- Pertemuan 12 - Tipe Data Dictionary/Latihan_11.1.py
+++
@@ -1,8 +0,0 @@
-data = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
-
-# Menampilkan judul
-print("key\tvalue\titem")
-
-# Melakukan perulangan pada dictionary
-for key, value in data.items():
-    print(key, "\t", value, "\t", key)
